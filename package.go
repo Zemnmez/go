a8d201febf116fe3d0906f3123f3608c28f8aa28
tshadwel <tshadwel@amazon.com>
2019-03-18 17:44:01 +0000

    init

---
+++ package.go
@@ -0,0 +1,8 @@
+// Package zemn.me is the root of the zemnmez/go monorepo.
+//
+// You are probably looking for something else.
+package zemn.me // import "zemn.me"
+
+import "fmt"
+
+func init() { fmt.Println("zemn.me") }
