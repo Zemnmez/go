9f8d6787bf99869a5a1d34ce4791bb7849c703eb
tshadwel <tshadwel@amazon.com>
2019-03-21 16:31:51 +0000

    big sigh: all together now!

--- debug/debug.go
+++ debug/debug.go
@@ -10,6 +10,6 @@ import (
 func Log(fmt string, args ...interface{}) { log.Printf(fmt, args...) }
 func Assert(test bool, format string, args ...interface{}) {
 	if !test {
-		panic(fmt.Sprintf(fmt, args...))
+		panic(fmt.Sprintf(format, args...))
 	}
 }
